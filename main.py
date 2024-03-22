6400a36ba9dd51ea21329b56fccf7c031861a6c9
EtienneMaire <148442799+EtienneMaire@users.noreply.github.com>
2024-03-22 09:10:50 +0100

    Added a function to find the corresponding word

--- main.py
+++ main.py
@@ -6,10 +6,28 @@ BACKGROUND = (0, 0, 0)
 
 dico = []
 
-def findWord():
-    pass
+def compareWords(word0, word1):
+    if not (len(word0) == len(word1)):
+        return False
+
+    for i in range(len(word0)):
+        if not (word0[i] == word1[i] or word0[i] == '*' or word1[i] == '*'):
+            return False
+
+    return True
+
+def findWord(word):
+    r = []
+
+    for item in dico:
+        if compareWords(item, word):
+            r.append(item)
+
+    return r
 
 def main():
+    global dico
+
     f = open("dico.txt", "r")
     dico = f.read().split('\n');
     f.close()
