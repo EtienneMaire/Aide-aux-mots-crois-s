4c7ce14c061c1752b88d32eb04878b58af472582
EtienneMaire <148442799+EtienneMaire@users.noreply.github.com>
2024-03-25 12:27:15 +0100

    Added typing

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ White = (255, 255, 255)
 Gray = (128, 128, 128)
 
 surface = 0
+word = ""
+
+pygame.font.init()
+font = pygame.font.SysFont('Calibri', 50)
+
+textSurface = 0
 
 def compareWords(word0, word1):
     if not (len(word0) == len(word1)):
@@ -33,9 +39,17 @@ def drawCase(x, y):
     pygame.draw.rect(surface, White, pygame.Rect(x, y, 60, 60))
     pygame.draw.rect(surface, Gray, pygame.Rect(x, y, 60, 60), 3)
 
+def renderWord(screen):
+    global textSurface
+
+    for i in range(len(word)):
+        textSurface = font.render(word[i], False, (0, 0, 0))
+        screen.blit(textSurface, (47 + 65 * i, 33))
+
 def main():
     global dico
     global surface
+    global word
 
     f = open("dico.txt", "r")
     dico = f.read().split('\n');
@@ -55,10 +69,21 @@ def main():
             if event.type == pygame.QUIT:
                 gameRunning = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    print(word)
+                elif event.key == pygame.K_BACKSPACE:
+                    word = word[:-1]
+                else:
+                    if len(word) < 14:
+                        word += event.unicode
+
         screen.fill(BACKGROUND)
 
         for i in range(14):
             drawCase(30 + 65 * i, 30)
+
+        renderWord(screen)
 
         pygame.display.flip()
 
