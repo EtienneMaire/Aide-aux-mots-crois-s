42403316c4154aaa4941493c1729217424762bc5
EtienneMaire <148442799+EtienneMaire@users.noreply.github.com>
2024-03-28 10:36:35 +0100

    Added scrolling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ lettres = "abcdefghijklmnopqrstuvwxyz*"
 WIDTH = 970
 HEIGHT = 768
 BACKGROUND = (200, 200, 200)
+SCROLL_SPEED = 30
 
 dico = []
 White = (255, 255, 255)
@@ -23,6 +24,8 @@ resultsTextSurface = 0
 
 results = []
 resultsText = ""
+
+scroll = 0
 
 def compareWords(word0, word1):
     if not (len(word0) == len(word1)):
@@ -44,22 +47,22 @@ def findWord(word):
     return r
 
 def drawCase(x, y):
-    pygame.draw.rect(surface, White, pygame.Rect(x, y, 60, 60))
-    pygame.draw.rect(surface, Gray, pygame.Rect(x, y, 60, 60), 3)
+    pygame.draw.rect(surface, White, pygame.Rect(x, y - scroll, 60, 60))
+    pygame.draw.rect(surface, Gray, pygame.Rect(x, y - scroll, 60, 60), 3)
 
 def drawResultsBG():
-    pygame.draw.rect(surface, LightGray, pygame.Rect(40, 100, 880, 650))
+    pygame.draw.rect(surface, LightGray, pygame.Rect(40, 100 - scroll, 880, max(650, 50 * len(resultsText.split('\n')))))
 
 def drawCursor(x, y):
     if pygame.time.get_ticks() % 1000 >= 500:
-        pygame.draw.rect(surface, Black, pygame.Rect(x, y, 25, 2))
+        pygame.draw.rect(surface, Black, pygame.Rect(x, y - scroll, 25, 2))
 
 def renderWord(screen):
     global textSurface
 
     for i in range(len(word)):
         textSurface = font.render(word[i], False, (0, 0, 0))
-        screen.blit(textSurface, (47 + 65 * i, 33))
+        screen.blit(textSurface, (47 + 65 * i, 33 - scroll))
 
 def renderResults(screen):
     global resultsTextSurface
@@ -67,13 +70,14 @@ def renderResults(screen):
     resultsText2 = resultsText.split('\n')
     for i in range(len(resultsText2)):
         resultsTextSurface = font.render(resultsText2[i], False, (0, 0, 0))
-        screen.blit(resultsTextSurface, (65, 120 + 50 * i))
+        screen.blit(resultsTextSurface, (65, 120 + 50 * i - scroll))
 
 def main():
     global dico
     global surface
     global word
     global resultsText
+    global scroll
 
     f = open("dico.txt", "r")
     dico = f.read().split('\n');
@@ -92,6 +96,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameRunning = False
+
+            if event.type == pygame.MOUSEWHEEL:
+                scroll -= event.y * SCROLL_SPEED
+                maxScroll = max(650, 120 + 50 * (len(resultsText.split('\n')) - 1)) - 650
+                scroll = max(min(scroll, maxScroll), 0)
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
